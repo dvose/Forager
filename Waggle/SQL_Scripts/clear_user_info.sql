7986411ca6042e9ab5cfd562328a4f562165721f
Dustin Vose <dtnvose@gmail.com>
2015-06-18 11:57:15 -0400

    add sql scripts

--- Waggle/SQL_Scripts/clear_user_info.sql
+++
@@ -1,6 +0,0 @@
-﻿use WaggleDb;
-delete from webpages_Membership;
-delete from webpages_UsersInRoles;
-delete from [File];
-delete from User_Profile;
-delete from [User];
---
+++ Waggle/SQL_Scripts/create_user_table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Email]       NVARCHAR (56) NOT NULL,
+    [IsSuspended] BIT           DEFAULT ((0)) NOT NULL,
+    [Avatar_Path] VARCHAR (MAX) NULL,
+    [Name]        VARCHAR (56)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([Email] ASC)
+);
+
