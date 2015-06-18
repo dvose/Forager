afc640648edbb6c1c40b7d74656c5109fd3c6025
Dustin Vose <dtnvose@gmail.com>
2015-06-18 11:47:46 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-# Waggle
+# Forager
 
-##Tasks
-- [x] Login
-- [x] File Management
-- [x] File Repository
-- [ ] Database
-- [x] Models
-- [ ] Admin Panel
-- [x] User Profile 
-- [x] Forums
-- [x] Topics
-- [x] Posts
-- [x] User Avatars
+## Setup
+1. create App_Data directory in your project (do not commit this as it has the database in it)
+2. Right click -> Add SQL Database -> name it ForagerDb.mdf
+3. Run the project (this should automatically create the user tables)
+
---
+++ Waggle/Controllers/UserController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using Forager.Models;
+using Forager.Filters;
+using System.IO;
+
+namespace Forager.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembershipAttribute]
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/Login
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (WebSecurity.IsAuthenticated) {
+                return RedirectToAction("Index","Forum");
+            }
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /User/Login
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid && WebSecurity.Login(model.Email, model.Password, persistCookie: model.RememberMe))
+            {
+                return RedirectToAction("Index", "Forum");
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+        //
+        // POST: /User/LogOff
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            WebSecurity.Logout();
+
+            return RedirectToAction("Login", "User");
+        }
+
+        //
+        // GET: /User/Register
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Register
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Attempt to register the user
+                try
+                {
+                    WebSecurity.CreateUserAndAccount(model.Email, model.Password, propertyValues: new { Name= model.Name}
+
+                    );
+                    Roles.AddUserToRole(model.Email, "normal");
+                    
+                    //create user profile
+                    using (UserEntitiesContext db = new UserEntitiesContext()) {
+                        var query = from a in db.Users
+                                    where a.Email.Contains(model.Email)
+                                    select a;
+                        User newUser = query.FirstOrDefault();
+
+                        //create user upload directory
+                        var strPath = Server.MapPath("~/Uploads/" + newUser.Id);
+                        Directory.CreateDirectory(strPath);
+                    }
+                
+                    WebSecurity.Login(model.Email, model.Password);
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("", ErrorCodeToString(e.StatusCode));
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "Email address already exists. Please enter a different email address.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The email provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+            }
+        }
+    }
+}
