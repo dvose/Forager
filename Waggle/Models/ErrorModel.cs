2cc8f1416ffe30af6c1c32c7a3422c99d4f8b77f
Bradley Richey <rrichey@spsu.edu>
2015-06-22 18:49:59 -0400

    Added Report and Error Models and controllers Added Report/Show table (without sorting)

---
+++ Waggle/Models/ErrorModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Forager.Models
+{
+    public class ErrorEntitiesContext : DbContext
+    {   
+        public ErrorEntitiesContext() : base("ForagerDb"){}
+        public DbSet<Forager.Models.ErrorModel> Errors { get; set; }
+    }
+
+    [Table("Error")]
+    public class ErrorModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ReportId { get; set; }
+        public string WebPage { get; set; }
+        public string ErrorStatus { get; set; }
+        public string Link { get; set; }
+    }
+
+    //Represents a list of all errors on a given page.
+    public class PageError
+    {
+        public int ReportId { get; set; }
+        public string Page { get; set; }
+        public List<ErrorModel> Errors { get; set; }
+    }
+}
---
+++ Waggle/Models/ReportModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Forager.Models
+{
+    public class ReportEntitiesContext : DbContext
+    {
+        public ReportEntitiesContext() : base("ForagerDb") { }
+        public DbSet<Forager.Models.ReportModel> Reports { get; set; }
+    }
+
+    [Table("Report")]
+    public class ReportModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public List<ErrorModel> Errors;
+    }
+}
