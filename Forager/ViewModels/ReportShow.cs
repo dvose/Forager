f5dd310c287caa3690ab38bc1cec37920e0bb870
Bradley Richey <rrichey@spsu.edu>
2015-06-26 13:47:49 -0400

    Added ReportShow

---
+++ Forager/ViewModels/ReportShow.cs
@@ -0,0 +1,14 @@
+﻿using Forager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forager.ViewModels
+{
+    public class ReportShow
+    {
+        public List<PageError> PageErrors;
+        public int SortType;
+    }
+}
