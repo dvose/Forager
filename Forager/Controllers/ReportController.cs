7a919876b3815c117fc093eb5fd37f43b8637ee3
Bradley Richey <rrichey@spsu.edu>
2015-07-06 19:16:26 -0400

    Added Report Comparisons

--- Forager/Controllers/ReportController.cs
+++ Forager/Controllers/ReportController.cs
@@ -22,74 +22,12 @@ namespace Forager.Controllers
             
             
         }
-
-        //Uncomment this after integration to keep from viewing without being logged in!
         [Authorize]
         public ActionResult Show(int ReportId, int SortType = 0)
         {
-            //Old code for sending a single report to the View.
-            /*
-            ReportEntitiesContext reports = new ReportEntitiesContext();
-            ReportModel r = reports.Reports.Find(ReportId);
-            if (r == null)
-            {
-                return View("ERROR REPORT NOT FOUND");
-            }
-            ErrorEntitiesContext db = new ErrorEntitiesContext();
-            r.Errors = new List<ErrorModel>();
-            foreach(ErrorModel err in db.Errors)
-            {
-                if(err.ReportId == ReportId)
-                {
-                    r.Errors.Add(err);
-                }
-            }
-            return View(r);
-             */
             if (PageError.CurrentReportPEs.Count == 0 || PageError.CurrentReportPEs[0].ReportId != ReportId)
             {
-                List<ErrorModel> ThisReportErrors;
-                using (ErrorEntitiesContext db = new ErrorEntitiesContext())
-                {
-                    ThisReportErrors = db.Errors.SqlQuery("SELECT * FROM Error WHERE ReportId = @p0", ReportId).ToList<ErrorModel>();
-                }
-                
-                //Debug.WriteLine("ThisReportErrors has " + ThisReportErrors.Count + " entries!");
-                List<PageError> PageErrors = new List<PageError>();
-                foreach (ErrorModel err in ThisReportErrors) //Go through every error on this report.
-                {
-                    bool PageFound = false;
-                    foreach (PageError pError in PageErrors) //Look through each PageError already created and see if this error is on that page.
-                    {
-                        if (err.WebPage == pError.Page)
-                        {
-                            pError.AddError(err); //Found a page for this error. Add it and continue to the next error.
-                            PageFound = true;
-                            break;
-                        }
-                    }
-                    if (!PageFound) //This page was not found, create a PageError for it and add it to that PageError.
-                    {
-                        PageError pe = new PageError()
-                        {
-                            ReportId = ReportId,
-                            Page = err.WebPage
-                        };
-                        pe.AddError(err);
-                        pe.Unfold = false;
-                        PageErrors.Add(pe);
-                    }
-                }
-                if (PageErrors.Count == 0) //If the list is empty (there are no errors), add a dummy one to avoid the View page breaking
-                {
-                    PageErrors.Add(new PageError()
-                    {
-                        ReportId = ReportId,
-                        Page = "Empty!"
-                    });
-                }
-                PageError.CurrentReportPEs = PageErrors;
-
+                PageError.CurrentReportPEs = BuildPageErrorList(ReportId);
             }
             //Copy pasted because lazy
             List<ErrorModel> ThisReportErrors2;
@@ -109,34 +47,12 @@ namespace Forager.Controllers
                 SortType = SortType,
                 Report = rep
             };
-            
-           switch(SortType){
-               case 1: //Sorted by error id. Default.
-                   rs.PageErrors.Sort(PageError.PageErrorID);
-                   break;
-               case 2: //Sort reports by number of errors on the report (descending)
-                   rs.PageErrors.Sort();
-                   break;
-               case 3: //Sort reports by number of errors on the report (ascending)
-                   rs.PageErrors.Sort(PageError.PageErrorAscending);
-                   break;
-               case 4: //Sort errors by error type
-                   foreach(PageError pe in rs.PageErrors)
-                   {
-                       pe.GetErrorList().Sort(ErrorModel.ErrorModelByErrorType);
-                   }
-                   break;
-               case 5: //Sort errors by broken link name
-                   foreach (PageError pe in rs.PageErrors)
-                   {
-                       pe.GetErrorList().Sort(ErrorModel.ErrorModelByLinkName);
-                   }
-                   break;
-               default: //0 or an invalid SortType was passed. Do no sorting.
-                   break;
-           }
+
+            DoSort(SortType, rs.PageErrors);
+           
             return View(rs);
         }
+        [Authorize]
         public ActionResult ToggleFold(int PEIndex, int SortType)
         {
             try
@@ -150,6 +66,135 @@ namespace Forager.Controllers
                 return null;
             }
         }
+        [Authorize]
+        public ActionResult BeginCompare(string ReportDropdown1, string ReportDropdown2)
+        {
+            int rep1 = Int32.Parse(ReportDropdown1);
+            int rep2 = Int32.Parse(ReportDropdown2);
+            return RedirectToAction("Compare", new { ReportId1 = rep1, ReportId2 = rep2, SortType = 0 });
+        }
+        [Authorize]
+        public ActionResult Compare(int ReportId1, int ReportId2, int SortType)
+        {
+            if(ReportCompare.CurrentCompare != null && ReportCompare.CurrentCompare.Report1.Report.Id == ReportId1 && ReportCompare.CurrentCompare.Report2.Report.Id == ReportId2)
+            {
+                return View(ReportCompare.CurrentCompare);
+            }
 
+            ReportCompare rc = new ReportCompare();
+            rc.Report1.PageErrors = BuildPageErrorList(ReportId1);
+            rc.Report2.PageErrors = BuildPageErrorList(ReportId2);
+            using(ReportEntitiesContext reports = new ReportEntitiesContext())
+            {
+                rc.Report1.Report = reports.Reports.Find(ReportId1);
+                rc.Report2.Report = reports.Reports.Find(ReportId2);
+                if (rc.Report1.Report == null || rc.Report2.Report == null)
+                {
+                    //Invalid ReportId. Redirect to index
+                    return RedirectToAction("Index");
+                }
+            }
+            if (SortType > 0)
+            {
+                DoSort(SortType, rc.Report1.PageErrors);
+                DoSort(SortType, rc.Report2.PageErrors);
+            }
+            ReportCompare.CurrentCompare = rc;
+            return View(rc);
+        }
+        [Authorize]
+        public ActionResult ToggleFoldCompare(bool Rep1, int PEIndex)
+        {
+            try
+            {
+                List<PageError> PEs = Rep1 ? ReportCompare.CurrentCompare.Report1.PageErrors : ReportCompare.CurrentCompare.Report2.PageErrors;
+                PEs[PEIndex].Unfold = !PEs[PEIndex].Unfold;
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                //PEIndex was invalid. Don't do anything special, just redirect back to the compare page.
+            }
+            return RedirectToAction("Compare", new { ReportId1 = ReportCompare.CurrentCompare.Report1.Report.Id, ReportId2 = ReportCompare.CurrentCompare.Report2.Report.Id, SortType = 0 });
+        }
+        [Authorize]
+        public ActionResult SortCompare(int SortType)
+        {
+            DoSort(SortType, ReportCompare.CurrentCompare.Report1.PageErrors);
+            DoSort(SortType, ReportCompare.CurrentCompare.Report2.PageErrors);
+            return RedirectToAction("Compare", new { ReportId1 = ReportCompare.CurrentCompare.Report1.Report.Id, ReportId2 = ReportCompare.CurrentCompare.Report2.Report.Id, SortType = 0 });
+        }
+
+        private static List<PageError> BuildPageErrorList(int ReportId)
+        {
+            List<PageError> PageErrors = new List<PageError>();
+            List<ErrorModel> ThisReportErrors;
+            using (ErrorEntitiesContext db = new ErrorEntitiesContext())
+            {
+                ThisReportErrors = db.Errors.SqlQuery("SELECT * FROM Error WHERE ReportId = @p0", ReportId).ToList<ErrorModel>();
+            }
+            foreach (ErrorModel err in ThisReportErrors) //Go through every error on this report.
+            {
+                bool PageFound = false;
+                foreach (PageError pError in PageErrors) //Look through each PageError already created and see if this error is on that page.
+                {
+                    if (err.WebPage == pError.Page)
+                    {
+                        pError.AddError(err); //Found a page for this error. Add it and continue to the next error.
+                        PageFound = true;
+                        break;
+                    }
+                }
+                if (!PageFound) //This page was not found, create a PageError for it and add it to that PageError.
+                {
+                    PageError pe = new PageError()
+                    {
+                        ReportId = ReportId,
+                        Page = err.WebPage
+                    };
+                    pe.AddError(err);
+                    pe.Unfold = false;
+                    PageErrors.Add(pe);
+                }
+            }
+            if (PageErrors.Count == 0) //If the list is empty (there are no errors), add a dummy one to avoid the View page breaking
+            {
+                PageErrors.Add(new PageError()
+                {
+                    ReportId = ReportId,
+                    Page = "Empty!"
+                });
+            }
+            PageError.CurrentReportPEs = PageErrors;
+            return PageErrors;
+        }
+        private static void DoSort(int SortType, List<PageError> PageErrors)
+        {
+            switch (SortType)
+            {
+                case 1: //Sorted by error id. Default.
+                    PageErrors.Sort(PageError.PageErrorID);
+                    break;
+                case 2: //Sort reports by number of errors on the report (descending)
+                    PageErrors.Sort();
+                    break;
+                case 3: //Sort reports by number of errors on the report (ascending)
+                    PageErrors.Sort(PageError.PageErrorAscending);
+                    break;
+                case 4: //Sort errors by error type
+                    foreach (PageError pe in PageErrors)
+                    {
+                        pe.GetErrorList().Sort(ErrorModel.ErrorModelByErrorType);
+                    }
+                    break;
+                case 5: //Sort errors by broken link name
+                    foreach (PageError pe in PageErrors)
+                    {
+                        pe.GetErrorList().Sort(ErrorModel.ErrorModelByLinkName);
+                    }
+                    break;
+                default: //0 or an invalid SortType was passed. Do no sorting.
+                    break;
+            }
+        }
     }
 }
---
+++ Forager/ViewModels/ReportCompare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forager.ViewModels
+{
+    public class ReportCompare
+    {
+        public static ReportCompare CurrentCompare;
+        public ReportShow Report1 { get; set; }
+        public ReportShow Report2 { get; set; }
+        public int SortType { get; set; }
+        public ReportCompare()
+        {
+            Report1 = new ReportShow();
+            Report2 = new ReportShow();
+        }
+    }
+
+}
